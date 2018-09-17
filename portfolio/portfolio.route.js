e461c6a39b0a1258b6bb3202a6e2129df61d9315
elainechan <elaine.chan@outlook.com>
2018-09-16 21:38:09 -0400

    Adds controller and route for updating portfolio upon buy/sell.

--- portfolio/portfolio.route.js
+++ portfolio/portfolio.route.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const controller = require('./portfolio.controller');
 
 router.get('/all', controller.getAllPortfolios);
+router.get('/update-one', controller.updateOnePortfolio);
 
 module.exports = router;
