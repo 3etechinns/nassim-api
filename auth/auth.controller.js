a1bfce0f230f32c1b1ea5805b3f602ae5c16cd7e
elainechan <elaine.chan@outlook.com>
2018-10-10 11:59:53 -0400

    Register and Login work after reinstalling bcrypt as bcryptjs.

--- auth/auth.controller.js
+++ auth/auth.controller.js
@@ -1,87 +1,80 @@
 'use strict';
 require('dotenv').config();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET;
 const Account = require('../account/account.model'); // without curly brackets
 
 exports.register = (req, res) => {
 	console.log(`req.body.email in auth.controller: ${req.body.email}`)
-	Account.find({	email: req.body.email })
-		.then(exists => {
-			console.log(`exists in Account.find(): ${exists}`)
-			console.log(`exists === true: ${exists === true}`);
-			if (exists !== '') {
-				res.status(307).json({
+
+	Account.findOne({	email: req.body.email }, (err, account) => {
+		if (err) {
+			res.status(500).json({message: "Something happened while registering."});
+			return;
+		}
+		if (account) {
+			res.status(307).json({
 					message: 'Account already exists.'
-				});
-				return;
-			} else {
-				let newAccount = new Account();
-				newAccount.email = req.body.email;
-				newAccount.hashPassword(password)
+			});
+			return;
+		}
+		let newAccount = new Account();
+		newAccount.email = req.body.email;
+		newAccount.hashPassword(req.body.password)
 					.then(hashed => {
-						newAccount.password = hashed;
-						newAccount.save()
-							.then(account => {
-								res.status(201).json({
-									message: 'The account has been created.',
-									data: account
-								});
-							})
-							.catch(err => {
-								res.status(500).json({
-									message: 'Something happened while saving account.'
-								});
-							});
+							newAccount.password = hashed;
+							newAccount.save()
+									.then(account => {
+											res.status(201).json({
+													message: 'The account has been created.',
+													data: account
+											});
+									})
+									.catch(err => {
+											res.status(500).json({
+													message: 'Something happened while saving account.'
+											});
+									});
 					})
 					.catch(err => {
-						res.status(500).json({
-							message: 'Something happened while encrypting password.'
-						});
+							res.status(500).json({
+									message: 'Something happened while encrypting password.'
+							});
 					});
-			}
-		})
-		.catch(err => {
-			res.status(500).json({
-				message: 'Something happened during the registration process.',
-				data: err
-			})
-		});
+	});
 }
 
 exports.login = (req, res) => {
-	Account.findOne({ email: req.body.email })
-		.then(account => {
-			if (!account) {
-				res.status(404).json({
-					message: 'User not found.'
-				});
-				return;
-			} else if (!account.comparePasswords(req.body.password)) { // passwords don't match
-				res.status(401).json({
-					message: 'Incorrect email or password.'
-				});
-				return;
-			} else {
-				let user = {
-					userId: user._id,
-					email: user.email
-				};
-				let token = jwt.sign(user, JWT_SECRET);
-				res.status(201).json({
-					message: 'Token has been generated.',
-					data: {
-						token: token,
-						email: user.email,
-						userId: user._id 
-					}
-				});
+	Account.findOne({ email: req.body.email }, (err, account) => {
+		if (err) {
+			res.status(500).json({message: "Something happened while logging in."});
+			return;
+		}  
+		if (!account) {
+			res.status(404).json({
+				message: 'User not found.'
+			});
+			return;
+		} 
+		if (!account.comparePasswords(req.body.password)) {
+			res.status(401).json({
+				message: 'Incorrect email or password.'
+			});
+			return;
+		}
+		let user = {
+			userId: account._id,
+			email: account.email
+		};
+		let token = jwt.sign(user, JWT_SECRET);
+		res.status(200).json({
+			message: 'Token has been generated.',
+			data: {
+				token: token,
+				email: account.email,
+				userId: account._id 
 			}
-		})
-		.catch(err => {
-			res.status(500).json({
-				message: 'Something happened during the login process.'
-			})
 		});
+	})
 }
